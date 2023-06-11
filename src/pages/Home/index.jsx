8ff0c0943f9939dde8472152ab8fc7fab797aa81
chihiro <chihiro365yb@gmail.com>
2023-06-11 16:13:49 +0800

    save

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './index.less';
 
 const Home = () => {
-    return <div>我是Home</div>;
+    const [toggle, setToggle] = useState(false);
+    return (
+        <>
+            {toggle ? (
+                <div className="child" onClick={() => setToggle(!toggle)}>
+                    124
+                </div>
+            ) : (
+                <div className="father" onClick={() => setToggle(!toggle)}>
+                    我是Home
+                </div>
+            )}
+        </>
+    );
 };
 
 export default Home;
